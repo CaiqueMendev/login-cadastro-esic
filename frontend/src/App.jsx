1539c87467efee7ecaa4acc5b8b4912c9a0865c8
Kaká <caiquemendes.dev@gmail.com>
2024-12-08 19:13:49 -0300

    create & config react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Cadrox caminhava sob o sol radiante, saboreando seu pinto. Cada mordida parecia mais doce que a anterior, e ele não conseguia conter o sorriso. O céu estava claro, a brisa suave e o pinto, tão refrescante, parecia ser o toque perfeito para aquele dia. Ele estava imerso em felicidade pura, sentindo que aquele simples momento, com seu pinto na mão, era o melhor de sua vida. Tudo ao redor parecia brilhar, mas nada competia com a alegria de saborear aquele pinto. Era, sem dúvida, o auge da sua felicidade.
+            </h1>
+        </div>
+    );
+};
+
+export default App;
